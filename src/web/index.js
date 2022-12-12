f1118d5dc80b94fbc938324c3877a3852860a458
maruson08 <maruson08@naver.com>
2022-12-12 18:48:03 +0900

    add songs

--- src/web/index.js
+++ src/web/index.js
@@ -8,9 +8,9 @@ $('.navBtn').click(function(){
                 const div = $('<div />', {
                     'id': key,
                     'class': 'musicDiv',
-                    'style': `margin:30%;margin-top:0;margin-bottom:10px;padding: 10px; background-color:${color};color:white`
-                }).get(0);
-                div.append(`${data[key]} - ${key}`);
+                    'style': `margin:30%;margin-top:0;margin-bottom:10px;padding: 10px;height:12px;overflow: hidden; background-color:${color};color:white`
+                });
+                div.append(`${data[key].artist} - ${key}<br><br>${data[key].discribe}`);
                 $('#main').append(div);
                 if(color == 'red'){
                     color = 'green';
