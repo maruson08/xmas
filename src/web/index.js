c21ab5769f0d5f5bde21169ad357bdb1ebe49b11
maruson08 <maruson08@naver.com>
2022-12-17 16:37:19 +0900

    add audio player

--- src/web/index.js
+++ src/web/index.js
@@ -8,7 +8,7 @@ $('.navBtn').click(function(){
                 const div = $('<div />', {
                     'id': key,
                     'class': 'musicDiv',
-                    'style': `margin:30%;margin-top:0;margin-bottom:10px;padding: 10px;height:12px;overflow: hidden; background-color:${color};color:white`
+                    'style': `background-color:${color};`
                 });
                 div.append(`${data[key]} - ${key}`);
                 $('#main').append(div);
@@ -26,9 +26,30 @@ $('.navBtn').click(function(){
     }
 })
 
-$('.musicDiv').click(function(){
+// /
+// $('.musicDiv').click(function(){
+//     const id = this.id;
+//     console.log(id);
+//     var audio = new Audio(`/music/${id}.mp3`);
+//     audio.play();
+// })
+
+
+let now_play_audio;
+$(document).on("click", ".musicDiv", function (){
+    $('.musicDiv').css('margin-left', '30%');
+    $('.musicDiv').css('margin-right', '30%');
+    $(this).css('margin-left', '27%');
+    $(this).css('margin-right', '27%');
+
     const id = this.id;
-    console.log(id);
-    var audio = new Audio(`/music/${id}.mp3`);
-    audio.play();
-})
+    if(now_play_audio == undefined){
+        now_play_audio = new Audio(`/music/${id}.mp3`);
+        now_play_audio.play();
+        
+    }else{
+        now_play_audio.pause();
+        now_play_audio = new Audio(`/music/${id}.mp3`);
+        now_play_audio.play();
+    }
+});
